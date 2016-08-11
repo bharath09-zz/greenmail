35c5f9d41700c3a3a687616d3741b5e7f8f89031
Bharath <bharath@revature.com>
2016-08-11 12:29:49 +0530

    2 # added junit test case
    just added junit test case

---
+++ greenmail-spring/com/properties/Emailer.java
@@ -0,0 +1,11 @@
+package com.properties;
+
+public class Emailer {
+
+  public static String localhost = "localhost";
+  public static int port = 3025;
+  public static String from = "from@codechef.in";
+  public static String to = "to@codechef.in";
+  public static String subject = "test subject";
+  public static String message = "test message";
+}
---
+++ greenmail-spring/test/com/email/EmailerTest.java
@@ -0,0 +1,70 @@
+package test.com.email;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static com.properties.Emailer.*;
+import java.io.IOException;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetupTest;
+
+public class EmailerTest {
+  private JavaMailSenderImpl emailSender;
+
+  private GreenMail testSmtp;
+
+  @Before
+  public void testSmtpInit() {
+    testSmtp = new GreenMail(ServerSetupTest.SMTP);
+    testSmtp.start();
+    emailSender = new JavaMailSenderImpl();
+
+    // don't forget to set the test port!
+    emailSender.setPort(port);
+    emailSender.setHost(localhost);
+
+    testSmtp.setUser(to, "123");
+  }
+
+
+
+  @Test
+  public void testSendAndVerifyingEmail()
+      throws InterruptedException, MessagingException, IOException {
+    SimpleMailMessage mail = new SimpleMailMessage();
+
+    mail.setFrom(from);
+    mail.setTo(to);
+    mail.setSubject(subject);
+    mail.setText(message);
+    assertNotNull(emailSender.getPort());
+
+    emailSender.send(mail);
+
+    // checking mail has sent or not through subject
+    Message[] messages = testSmtp.getReceivedMessages();
+    assertNotNull(testSmtp.getReceivedMessages());
+    assertEquals("test subject", messages[0].getSubject());
+
+  }
+
+  @Test
+  public void zzz() {
+      
+  }
+
+
+  @After
+  public void cleanup() {
+    testSmtp.stop();
+  }
+}
